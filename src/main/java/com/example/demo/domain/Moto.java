ec976016431444e2d90c243c7cbcfff302fa7ea7
Carol Ribeiro <lxvcarol@gmail.com>
2024-07-25 18:16:54 -0300

    conexão com o banco de dados e criação da tabela Moto

---
+++ src/main/java/com/example/demo/domain/Moto.java
@@ -0,0 +1,49 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name = "moto_tbl")
+public class Moto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @NotBlank(message = "Marca não pode estar vazia")
+    private String marca;
+
+    @NotBlank(message = "Modelo não pode estar vazio")
+    private String modelo;
+
+    @Positive(message = "O ano precisa ser um número positivo")
+    @NotNull(message = "Ano não pode ser nulo")
+    private Integer ano;
+
+    @Positive(message = "Preço tem que ser um número positivo")
+    @NotNull(message = "Preço não pode ser nulo")
+    private Double preco;
+
+    @NotBlank(message = "A capacidade do motor não pode estar vazia")
+    private String capacidadeMotor;
+
+    private boolean isDeleted = false;
+
+    private String imageUri;
+
+    public void regrasDeNegocioParaCadastro() {
+        marca.toUpperCase();
+    }
+}
